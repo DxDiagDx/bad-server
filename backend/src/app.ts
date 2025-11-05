42084585d967691392b6d48e20e9f829a677f8dc
Evgeny Lukin <lukin@usota.ru>
2025-11-05 21:02:38 +0300

    fix: adjust rate limiting order and limits

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,6 +29,7 @@ app.use(serveStatic(path.join(__dirname, 'public')))
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.options('*', cors())
+app.use(limiter);
 app.use(routes)
 
 const csrfProtection = csurf({
--- backend/src/middlewares/rate-limit.ts
+++ backend/src/middlewares/rate-limit.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 100 requests per windowMs
+  max: 50, // limit each IP to 100 requests per windowMs
   message: {
     error: "Слишком много запросов с этого IP-адреса. Повторите попытку позже"
   },
